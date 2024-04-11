8aa11cdb976200889a1cbec6993e21d2e5265175
Philip Obi <philipobiu@gmail.com>
2024-04-11 02:59:00 +0200

    implementing sparse matrix constructor and destructor

--- src/linalg.c
+++ src/linalg.c
@@ -2,70 +2,128 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define square(a) ((a) * (a))
+#define EPSILON 0.01
 
-typedef struct coo_mat {
-    int n, m, nnz;
-    int *row_coords, *col_coords;
+
+
+typedef struct sparse_row {
+    int nnz;
+    int *col;
     sfloat *data;
-} coo_mat;
+} sparse_row;
 
-void coo_to_arr(const coo_mat* matrix, sfloat *arr)
+typedef struct sparse_matrix {
+    int n, m;
+    sparse_row **rows;
+    sparse_row *row_mem;
+} sparse_matrix;
+
+sparse_matrix *init(int n, int m)
 {
-    int 
-    *p_row_coord = matrix->row_coords,
-    *p_col_coord = matrix->col_coords,
-    m = matrix->m;
-    sfloat *p_data = matrix->data;
+    sparse_matrix *matrix = malloc(sizeof(sparse_matrix)); 
+    sparse_row **p_rows = malloc(n * sizeof(sparse_row *));
+    sparse_row *rows = malloc(n * sizeof(sparse_row));
 
-    for(int k = 0; k < matrix->nnz; k++, p_row_coord++, p_col_coord++, p_data++) {
-        arr[*p_row_coord * m + *p_col_coord] = *p_data;
+    if (!matrix || !p_rows || !rows) goto error;
+    
+    int i;
+    sparse_row **p_rows_c = p_rows;
+    sparse_row *p_row = rows;
+    for (i = 0; i < n; i++, p_row++, p_rows_c++) {
+        p_row->col = malloc(m * sizeof(int));
+        p_row->data = malloc(m * sizeof(sfloat));
+        if (!p_row->col || !p_row->data) goto cleanup;
+        p_row->nnz = 0;
+        *p_rows_c = p_row;
     }
+    
+    matrix->rows = p_rows;
+    matrix->row_mem = rows;
+    return matrix;
+
+    cleanup:
+    p_rows_c = p_rows;
+    p_row = rows;
+    for (int k = 0; k < i; k++, p_row++, p_rows_c++) {
+        *p_rows_c = NULL;
+        free(p_row->col);
+        free(p_row->data);
+    }
+    matrix->rows = NULL;
+    
+    error:
+    free(p_rows);
+    free(rows);
+    free(matrix);
+    return NULL;
 }
 
-void print_arr(const sfloat *arr, int n, int m)
+void unref(sparse_matrix *matrix)
 {
-    int i,j;
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < m; j++) {
-            printf("%2.0f    ", arr[i*m + j]);
+    sparse_row **p_rows = matrix->rows;
+    sparse_row *p_row;
+    for(int i = 0; i < matrix->n; i++) {
+        if ((p_row = *p_rows) == NULL) continue;
+        free(p_row->col);
+        free(p_row->data);
+        *p_rows++ = NULL;
+    }
+    free(matrix->rows[0]);
+}
+
+sfloat sparse_mul_rows(const sparse_row *p_row_i, const sparse_row *p_row_j)
+{
+    int n, k, m=0;
+    sfloat 
+    sum = 0,
+    *data1, *data2;
+    for (n = 0; n < p_row_i->nnz; n += 3) {
+        for (; m < p_row_j->nnz; m += 3) {
+            if (p_row_i->col[n] < p_row_j->col[m]) break;
+            if (p_row_i->col[n] > p_row_j->col[m]) continue;
+            data1 = p_row_i->data + n;
+            data2 = p_row_j->data + m;
+            for(k = 0; k < 3; k++) sum += (*data1++) * (*data2++);
         }
-        printf("\n");
     }
 }
 
-void print_coo(const coo_mat *matrix)
+void sparse_mul_transpose(const sparse_matrix *matrix, sparse_matrix *result_matrix)
 {
-    int
-    n = matrix->n,
-    m = matrix->m;
-    sfloat *arr = (sfloat *) malloc(n * m * sizeof(sfloat));
-    if(arr == NULL) return;
+    int i, j, k, p, n_blk,
+    n = matrix->n;
+    sparse_row 
+    *p_row_i, *p_row_j,
+    *p_rrow_i, *p_rrow_j,
+    **rows = matrix->rows,
+    **rrows = result_matrix->rows;
+    sfloat sum;
 
-    coo_to_arr(matrix, arr);
-    print_arr(arr, n, m);
-
-    free(arr);
+    for(i = 0; i < n; i++) {
+        if ((p_row_i = rows[i]) == NULL) continue; // row i empty
+        // j < i
+        for(j = 0; j < i; j++) {
+            if ((p_row_j = rows[j]) == NULL) continue; // row j empty
+            sum = sparse_mul_rows(p_row_i, p_row_j);
+            if (sum < EPSILON || -sum < EPSILON) continue; // -> 0
+            p_rrow_i = rrows[i];
+            p_rrow_i->col[p_rrow_i->nnz] = j;
+            p_rrow_i->data[p_rrow_i->nnz++] = sum;
+            
+            p_rrow_j = rrows[j];
+            p_rrow_j->col[p_rrow_j->nnz] = i;
+            p_rrow_j->data[p_rrow_j->nnz++] = sum;
+            
+        }
+        // j = i
+        sum = 0;
+        for (k = 0; k < p_row_i->nnz; k++) sum += square(p_row_i->data[k]);
+        if (sum < EPSILON || -sum < EPSILON) continue; // -> 0
+        p_rrow_i = rrows[i];
+        p_rrow_i->col[p_rrow_i->nnz] = i;
+        p_rrow_i->data[p_rrow_i->nnz++] = sum;
+    }
 }
 
-void test()
-{
-    int row_coords[] = {0,0,1,1,2,2,3,3};
-    int col_coords[] = {0,1,0,1,2,3,2,3};
-    sfloat data[] = {1,2,3,4,5,6,7,8};
-    coo_mat matrix = {
-        .n = 4,
-        .m = 4,
-        .nnz = 8,
-        .row_coords = row_coords,
-        .col_coords = col_coords,
-        .data = data
-    };
-    print_coo(&matrix);
-}
-
-
-int main()
-{
-    test();
-    return 0;
-}
--- src/main.c
+++ src/main.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
-#include <wchar.h>
+
+
+// sfloat mul_rows(const sparse_row *p_row_i, const sparse_row *p_row_j)
+// {
+//     int n, k, m=0;
+//     sfloat 
+//     sum = 0,
+//     *data1, *data2;
+//     for (n = 0; n < p_row_i->nnz; n += 3) {
+//         for (; m < p_row_j->nnz; m += 3) {
+//             if (p_row_i->col[n] < p_row_j->col[m]) break;
+//             if (p_row_i->col[n] > p_row_j->col[m]) continue;
+//             data1 = p_row_i->data + n;
+//             data2 = p_row_j->data + m;
+//             for(k = 0; k < 3; k++) sum += (*data1++) * (*data2++);
+//         }
+//     }
+// }
 
 int main()
 {
-    wchar_t a = L'𝒶';
-    char *mystring = "hello";
-    wchar_t out;
-    for (char *p_char = mystring; *p_char != '\0'; p_char++) {
-        out = a + (*p_char - 'a');
-        wprintf(L"%lc", a);
+    int row1[] = {
+        0,1,2,
+        6,7,8,
+        9,10,11
+    };
+    int row2[] = {
+        3,4,5,
+        6,7,8,
+        12,13,14
+    };
+
+    int 
+    m=0,
+    *data1, *data2;
+
+    for (int n=0; n < 9; n+=3) {
+        for (; m<9; m+=3) {
+            if(row1[n] < row2[m]) break;
+            if(row1[n] > row2[m]) continue;
+            data1 = row1 + n;
+            data2 = row2 + m;
+            for (int k=0; k<3; k++) printf("(%i,%i)\n",*data1++,*data2++);
+        }
     }
     return 0;
 }
